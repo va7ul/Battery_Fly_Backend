d16f3fe06b5165fc2a5e95968fcd39e700d65411
dmazepa2017 <dmytro.mazepa@gmail.com>
2024-05-03 23:17:30 +0300

    add getPromoCode

--- routes/api/price3d.js
+++ routes/api/price3d.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { validateBody } = require('../../middlewares');
-const { schemas } = require('../../models/order');
 const ctrl = require('../../controllers/price3d');
 
 const router = express.Router();
